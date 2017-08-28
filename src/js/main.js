637cd29828cd01df194135f76abb43282e0a09d1
kevindantas <kevin.ds.29@gmail.com>
2017-08-28 16:15:32 -0300

    Select course style

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,18 @@ var menu = document.querySelector('header');
 toggleMenu.addEventListener('click', (e) => {
   menu.classList.toggle('active');
 });
+
+var selectCoursesContainer = document.querySelector('#select-course');
+var selectCoursesButton = document.querySelectorAll('#select-course ul li a');
+
+selectCoursesButton.forEach((item) => {
+  item.addEventListener('click', selectCourse);
+});
+
+
+function selectCourse(e) {
+  console.log();
+  var targetElem = document.querySelector(e.currentTarget.hash);
+  selectCoursesContainer.classList.add('active');
+  targetElem.classList.add('active');
+}
