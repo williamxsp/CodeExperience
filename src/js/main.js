babb659caef768a14fb006c8c06757c397267c3a
kevindantas <kevin.ds.29@gmail.com>
2017-08-28 15:11:08 -0300

    Make toggle menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@ var mySwiper = new Swiper('.swiper-container', {
   speed: 400,
   spaceBetween: 100
 });   
+
+
+var toggleMenu = document.querySelector('.toggle-menu');
+var menu = document.querySelector('header');
+
+toggleMenu.addEventListener('click', (e) => {
+  menu.classList.toggle('active');
+});
